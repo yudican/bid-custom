6fb0596416f98155ee0110194c8e3c88c048d8f1
Yudi Candra <yudhichandra02@gmail.com>
2023-10-08 23:12:37 +0700

    update build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vite"
+import laravel from "laravel-vite-plugin"
+import react from "@vitejs/plugin-react"
+import svgr from "vite-plugin-svgr"
 
-const host = "https://crm-aimigroup.test";
+const host = "https://crm-aimigroup.test"
 export default defineConfig({
   plugins: [
-    laravel(["resources/css/app.css", "resources/js/app.js"]),
+    // laravel(["resources/css/app.css", "resources/js/app.js"]),
     svgr(),
     react(),
   ],
-});
+})
